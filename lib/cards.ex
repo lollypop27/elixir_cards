e649c9264d7925a89ff03eac27f7e713ff2612fc
lollypop27 <lara@adafctory.co.za>
2017-03-22 07:41:38 +0200

    Working with pipe operators and writing documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,38 +1,62 @@
 defmodule Cards do
+  @moduledoc """
+  Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+  Returens a list of strings representing a deck of playing cards
+  """
 
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-    
+
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
   end
-  
+
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
-  
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
-  
+
+  @doc """
+  Divides a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+
+        iex> deck =Cards.create_deck
+        iex> {hand, deck} =  Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+
+  """
+
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
-  
+
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck) # invoke erlang code, has built in methods, takes deck and converts it to an object
     File.write(filename, binary) # then passed in
   end
-  
+
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
       {:error, _reason} -> "That file does not exist"
     end
   end
-  
-  
-  
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
 end
